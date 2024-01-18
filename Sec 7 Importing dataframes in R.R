7c3eb0941ecfd7284c6f1cd1e3582de18abd650a
Siddhant Ghosh <141497195+SiddhantIQByte@users.noreply.github.com>
2024-01-19 01:31:22 +0530

    Add files via upload

---
+++ Sec 7 Importing dataframes in R.R
@@ -0,0 +1,9 @@
+# my.pok <- read.table("pokRdex_comma.csv",sep=',',
+#                       header=TRUE,
+#                       stringsAsFactors = FALSE)
+
+my.pok <- read.csv("pokRdex_comma.csv",
+                      stringsAsFactors = FALSE)
+my.pok
+# ?read.delim
+
